01fcddbc9d119e4945f776cc85f2e24bab0e4295
Marek Wieiwórka <marek.wiewiorka@gmail.com>
2025-02-17 21:32:31 +0100

    Refactor

--- datafusion/vcf/src/physical_exec.rs
+++ datafusion/vcf/src/physical_exec.rs
@@ -56,7 +56,7 @@ fn build_record_batch(
 
 
 
-async fn get_local_vcf(file_path: String, schema_ref: SchemaRef, batch_size: usize, thread_num: usize) -> datafusion::error::Result<AsyncStream<datafusion::error::Result<RecordBatch>, impl Future<Output=()> + Sized>> {
+async fn get_local_vcf(file_path: String, schema_ref: SchemaRef, batch_size: usize, thread_num: Option<usize>)  -> datafusion::error::Result<impl futures::Stream<Item = datafusion::error::Result<RecordBatch>>> {
     let mut chroms: Vec<String> = Vec::with_capacity(batch_size);
     let mut poss: Vec<u32> = Vec::with_capacity(batch_size);
     let mut ids: Vec<String> = Vec::with_capacity(batch_size);
@@ -69,14 +69,12 @@ async fn get_local_vcf(file_path: String, schema_ref: SchemaRef, batch_size: usi
     let mut batch_num = 0;
     let schema = Arc::clone(&schema_ref);
     let file_path = file_path.clone();
-    // let worker_count = thread::(NonZero::new(1).unwrap());
+    let thread_num = thread_num.unwrap_or(1);
     let mut reader = File::open(file_path)
         .map(|f| noodles_bgzf::MultithreadedReader::with_worker_count(NonZero::new(thread_num).unwrap(), f))
-        .map(vcf::io::Reader::new).unwrap();
-    let header = reader.read_header().unwrap();
-
-
-    let iter = std::iter::from_fn(move || {
+        .map(vcf::io::Reader::new)?;
+    let header = reader.read_header()?;
+    let iter  = std::iter::from_fn(move || {
 
         let mut records = reader.records();
         while count < batch_size {
@@ -112,7 +110,7 @@ async fn get_local_vcf(file_path: String, schema_ref: SchemaRef, batch_size: usi
         batch_num += 1;
         Some(Ok(batch))
     });
-    stream::iter(iter)
+    Ok(stream::iter(iter))
 }
 
 
@@ -193,167 +191,27 @@ async fn get_remote_vcf_stream(file_path: String, schema: SchemaRef, batch_size:
 }
 
 
-async fn get_stream(file_path: String, schema_ref: SchemaRef, batch_size: usize) -> datafusion::error::Result<SendableRecordBatchStream> {
+async fn get_stream(file_path: String, schema_ref: SchemaRef, batch_size: usize, thread_num: Option<usize>) -> datafusion::error::Result<SendableRecordBatchStream> {
     // Open the BGZF-indexed VCF using IndexedReader.
-    let schema = Arc::clone(&schema_ref);
+
     let file_path = file_path.clone();
-
-
-
     let store_type = get_storage_type(file_path.clone());
-    let stream = match store_type {
-        StorageType::GCS => get_remote_vcf_stream(file_path.clone(), schema.clone(), batch_size).await?,
-        _ => panic!("Unsupported storage type")
-    };
-
-
-    // let stream = reader.records().into_stream().chunks(batch_size).map(move |chunk| {
-    //         for r in chunk {
-    //             let record = r.unwrap();
-    //             chroms.push(record.reference_sequence_name().to_string());
-    //             poss.push(record.variant_start().unwrap().unwrap().get() as u32 + 1);
-    //             ids.push(".".to_string());
-    //             refs.push(record.reference_bases().to_string());
-    //             alts.push(".".to_string());
-    //             quals.push(record.quality_score().unwrap_or(Ok(0.0)).unwrap() as f64);
-    //             filters.push(".".to_string());
-    //             count += 1;
-    //         };
-    //         println!("Batch number: {}", batch_num);
-    //         let batch = build_record_batch(Arc::clone(&schema), &chroms, &poss, &ids, &refs, &alts, &quals, &filters)?;
-    //         count = 0;
-    //         chroms.clear();
-    //         poss.clear();
-    //         ids.clear();
-    //         refs.clear();
-    //         alts.clear();
-    //         quals.clear();
-    //         filters.clear();
-    //         batch_num += 1;
-    //         Ok(batch)
-    //     }
-    // );
-
-    // let stream = records.m
-
-    // let iter = std::iter::from_fn(move  || {
-    //     let mut records = reader.records()
-    //     while count < batch_size {
-    //         let record = records.next();
-    //         record_num += 1;
-    //         println!("Record number: {}", record_num);
-    //         let record = block_on(record);
-    //         if record.is_none() {
-    //             break;
-    //         }
-    //         let record = record.unwrap().unwrap();
-    //         // For each record, fill the fixed columns.
-    //         chroms.push(record.reference_sequence_name().to_string());
-    //         poss.push(record.variant_start().unwrap().unwrap().get() as u32 + 1);
-    //         ids.push(".".to_string());
-    //         refs.push(record.reference_bases().to_string());
-    //         alts.push(".".to_string());
-    //         quals.push(record.quality_score().unwrap_or(Ok(0.0)).unwrap() as f64);
-    //         filters.push(".".to_string());
-    //         count += 1;
-    //     }
-    //     if count == 0 {
-    //         return None;
-    //     }
-    //     println!("Batch number: {}", batch_num);
-    //     let batch = build_record_batch(Arc::clone(&schema), &chroms, &poss, &ids, &refs, &alts, &quals, &filters).unwrap();
-    //     count = 0;
-    //     chroms.clear();
-    //     poss.clear();
-    //     ids.clear();
-    //     refs.clear();
-    //     alts.clear();
-    //     quals.clear();
-    //     filters.clear();
-    //     batch_num += 1;
-    //     Some(Ok(batch))
-    // });
-    // let stream = stream::iter(iter);
     let schema = schema_ref.clone();
-    Ok(Box::pin(RecordBatchStreamAdapter::new(schema_ref, stream)))
 
+    match store_type {
+        StorageType::LOCAL => {
+            let stream = get_local_vcf(file_path.clone(), schema.clone(), batch_size, thread_num).await?;
+            Ok(Box::pin(RecordBatchStreamAdapter::new(schema_ref, stream)))
+        },
+        StorageType::GCS => {
+            let stream = get_remote_vcf_stream(file_path.clone(), schema.clone(), batch_size).await?;
+            Ok(Box::pin(RecordBatchStreamAdapter::new(schema_ref, stream)))
+        },
+        _ => panic!("Unsupported storage type")
+    }
 
 }
 
-
-// async fn get_stream(file_path: String, schema_ref: SchemaRef, batch_size: usize) -> datafusion::error::Result<SendableRecordBatchStream> {
-//     // Open the BGZF-indexed VCF using IndexedReader.
-//     let mut chroms: Vec<String> = Vec::with_capacity(batch_size);
-//     let mut poss: Vec<u32> = Vec::with_capacity(batch_size);
-//     let mut ids: Vec<String> = Vec::with_capacity(batch_size);
-//     let mut refs: Vec<String> = Vec::with_capacity(batch_size);
-//     let mut alts: Vec<String> = Vec::with_capacity(batch_size);
-//     let mut quals: Vec<f64> = Vec::with_capacity(batch_size);
-//     let mut filters: Vec<String> = Vec::with_capacity(batch_size);
-//     let mut count: usize = 0;
-//     let mut record_num = 0;
-//     let mut batch_num = 0;
-//     let schema = Arc::clone(&schema_ref);
-//     let file_path = file_path.clone();
-//     let mut reader = match file_path {
-//         file_path if file_path.starts_with("gs://") | file_path.starts_with("s3://") => Reader::from_url(&Url::parse(&*file_path.clone()).unwrap()),
-//         _ => Reader::from_path(file_path.clone())
-//     }.unwrap();
-//     reader.set_threads(1);
-//     let mut reader = Arc::new(Mutex::new(reader));
-//     let iter = std::iter::from_fn(move || {
-//         let mut reader = reader.lock().unwrap();
-//         let header = reader.header().clone();
-//         let mut records = reader.records();
-//         while count < batch_size {
-//             let record = records.next();
-//             record_num += 1;
-//            if  record.is_none() {
-//                break;
-//            }
-//             let record = record.unwrap().unwrap();
-//             let contig_id = header.rid2name(record.rid().unwrap()).unwrap();
-//             // For each record, fill the fixed columns.
-//             chroms.push(String::from_utf8_lossy(contig_id).to_string());
-//             poss.push(record.pos() as u32 + 1);
-//             ids.push(".".to_string());
-//             let alleles = record.alleles();
-//             if let Some(r) = alleles.get(0) {
-//                 refs.push(String::from_utf8_lossy(r).to_string());
-//             } else {
-//                 refs.push("".to_string());
-//             }
-//             if alleles.len() > 1 {
-//                 let alt_vec: Vec<String> = alleles.iter().skip(1)
-//                     .map(|a| String::from_utf8_lossy(a).to_string())
-//                     .collect();
-//                 alts.push(alt_vec.join(","));
-//             } else {
-//                 alts.push("".to_string());
-//             }
-//             quals.push(record.qual() as f64);
-//             filters.push(if record.filters().count() == 0 { "PASS".to_string() } else { ".".to_string() });
-//             count += 1;
-//         }
-//         if count == 0 {
-//             return None;
-//         }
-//         println!("Batch number: {}", batch_num);
-//         let batch = build_record_batch(Arc::clone(&schema), &chroms, &poss, &ids, &refs, &alts, &quals, &filters).unwrap();
-//         count = 0;
-//         chroms.clear();
-//         poss.clear();
-//         ids.clear();
-//         refs.clear();
-//         alts.clear();
-//         quals.clear();
-//         filters.clear();
-//         batch_num += 1;
-//         Some(Ok(batch))
-//     });
-//     let stream = stream::iter(iter);
-//     Ok(Box::pin(RecordBatchStreamAdapter::new(schema_ref, stream)))
-// }
 
 
 pub struct VcfExec {
@@ -364,6 +222,7 @@ pub struct VcfExec {
     pub(crate) format_fields: Vec<String>,
     pub(crate) cache: PlanProperties,
     pub(crate) limit: Option<usize>,
+    pub(crate) thread_num: Option<usize>,
 }
 
 
@@ -376,6 +235,7 @@ impl VcfExec {
         format_fields: Vec<String>,
         cache: PlanProperties,
         limit: Option<usize>,
+        thread_num: Option<usize>,
     ) -> Self {
         Self {
             file_path: file_path.clone(),
@@ -385,6 +245,7 @@ impl VcfExec {
             format_fields,
             cache,
             limit,
+            thread_num,
         }
     }
 }
@@ -432,7 +293,7 @@ impl ExecutionPlan for VcfExec {
     fn execute(&self, _partition: usize, context: Arc<TaskContext>) -> datafusion::common::Result<SendableRecordBatchStream> {
         let batch_size = context.session_config().batch_size();
         let schema = self.schema.clone();
-        let fut = get_stream(self.file_path.clone(), schema.clone(), batch_size);
+        let fut = get_stream(self.file_path.clone(), schema.clone(), batch_size, self.thread_num);
         let stream = futures::stream::once(fut).try_flatten();
         Ok(Box::pin(RecordBatchStreamAdapter::new(schema, stream)))
 
