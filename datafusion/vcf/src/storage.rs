ccb5a9f2031a216ac974e6d34afec3a974486819
Piotr Dębski <ppdebski@interia.eu>
2025-06-01 13:37:17 +0200

    Update storage.rs
    fix method name disable_vm_metadata -> disable_ec2_metadata

--- datafusion/vcf/src/storage.rs
+++ datafusion/vcf/src/storage.rs
@@ -160,7 +160,7 @@ pub async fn get_remote_stream(
         StorageType::GCS => {
             let builder = Gcs::default()
                 .bucket(bucket_name.as_str())
-                .disable_vm_metadata()
+                .disable_ec2_metadata()
                 .allow_anonymous();
             let operator = Operator::new(builder)?
                 .layer(TimeoutLayer::new().with_io_timeout(std::time::Duration::from_secs(120)))
